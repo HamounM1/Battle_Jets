9a6652f47a49e0e50ab5010c237e19e234f1201b
tharane242 <71242467+tharane242@users.noreply.github.com>
2021-05-13 13:56:29 +0930

    started on functions

---
+++ src/game.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+#ifndef "GAME_H"
+
+
+
+    void runGame();
+    void playerIn(unsigned short positions[i][j]); //only takes in forward, climb or descent or attack as input.
+    void arrayPositions
+
